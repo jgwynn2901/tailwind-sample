e56f45187aaaa97295f1f82315b13af0ea641c6d
John Gwynn <jgwynn2901@yahoo.com>
2020-07-29 12:46:50 -0400

    Initial revision

---
+++ README.md
@@ -0,0 +1,10 @@
+# quick tailwind learning project
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run build 
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
